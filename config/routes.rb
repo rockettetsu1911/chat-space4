4d31d433049a5f95cc8369ee42cad2311d71c2e2
rockettetsu1911 <rockettetsu@gmail.com>
2020-04-12 10:35:55 +0900

    create_scss

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
   get 'messages/index'
 
+<<<<<<< Updated upstream
   root "messages#index"
+=======
+  root 'groups#index'
+  resources :users, only: [:edit, :update]
+  resources :groups, only: [:index, :new, :create, :edit, :update]
+>>>>>>> Stashed changes
 end
